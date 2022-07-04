f5fd997dc698fff3349e1bd981f684eed5762883
rfolk4835 <reid_folk@yahoo.ca>
2022-07-03 19:23:02 -0700

    Moved user controller

---
+++ interviewproject/users/controllers.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+# Start
+# 'index' should return json response for each user, combining 'first_name' and 'last_name'
+# exclude is_admin from the response
+
+
+
+# Feature
+# Support passing a parameter 'params[:search]' to query on 'last_name'
