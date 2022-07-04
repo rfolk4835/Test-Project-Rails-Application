29c622a3c9dbae6b9d741256224e73f00e3b2cd4
rfolk4835 <reid_folk@yahoo.ca>
2022-07-03 19:11:57 -0700

    Moved users model to interviewproject

--- interviewproject/users/models.py
+++ interviewproject/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 # Create your models here.
+
+# Start
+# Intialized user with 4 variables
+# Equivalent to 'database_design.sql'
+
+# AUTH_USER_MODEL = 'models.users'
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    born_on = models.DateField()
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'first_name'
+    REQUIRED_FIELDS = []
+
+    
+
+    def __str__(self):
+        return self.first_name
