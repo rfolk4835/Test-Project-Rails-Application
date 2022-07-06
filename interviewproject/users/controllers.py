14df6894693e551ca0302b89de55a1d075200c0f
rfolk4835 <reid_folk@yahoo.ca>
2022-07-06 00:42:45 -0700

    Developed CustomUserController, made CreateUser and CreateAdmin

--- interviewproject/users/controllers.py
+++ interviewproject/users/controllers.py
@@ -1,11 +1,19 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserController(BaseUserManager):
-    def CreateUser(self, first_name, last_name, born_on, is_admin):
-        pass
+    def CreateUser(self, username, password, first_name, last_name, born_on, **extrafields):
+        user = self.model(username = username, first_name = first_name, 
+                            last_name = last_name, born_on = born_on, **extrafields)
+        user.set_password(password)
+        user.save()
+        
+        return user
 
-    def CreateAdmin(self, first_name, last_name, born_on, is_admin):
-        pass
+    def CreateAdmin(self, username, password, first_name, last_name, born_on, **extrafields):
+        extrafields.setdefault('is_admin', True)
+        extrafields.setdefault('is_superuser', True)
+
+        return self.CreateUser(username, password, first_name, last_name, born_on, **extrafields)
 
 
 
