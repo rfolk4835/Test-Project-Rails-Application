dc84af20ac53f9e2abfbc27729868f9798bd7e90
rfolk4835 <reid_folk@yahoo.ca>
2022-07-06 00:24:02 -0700

    Initialized tests, resgistered CustomUser under admin

--- interviewproject/users/tests.py
+++ interviewproject/users/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth import get_user_model
 
 # user_controller response is :ok
-
 # is_admin isn't returning json reponses
+
+class usersTests(TestCase):
+
+    def test_ControllerResponse(self):
+        pass
+    
+    def test_JsonResponse(self):
+        pass
+
+
