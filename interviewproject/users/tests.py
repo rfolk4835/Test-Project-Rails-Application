a4bc39895dcf7646aeeec3060f1e8bb4fa66fcd3
rfolk4835 <reid_folk@yahoo.ca>
2022-07-07 23:48:23 -0700

    Changed users urls, added setup to tests

--- interviewproject/users/tests.py
+++ interviewproject/users/tests.py
@@ -1,15 +1,19 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from .models import CustomUser
 
 # user_controller response is :ok
 # is_admin isn't returning json reponses
 
 class usersTests(TestCase):
+    def test_setup(self):
+        CustomUser.objects.create(first_name = 'John', last_name = 'Smith', born_on = '1988-04-18')
+        CustomUser.objects.create(first_name = 'Susan', last_name = 'Williams', born_on = '1972-08-03', is_admin = True)
 
-    def test_ControllerResponse(self):
+    def test_ControllerResponse_ok(self):
         pass
     
-    def test_JsonResponse(self):
+    def test_JsonResponse_is_admin(self):
         pass
+
 
 
--- interviewproject/users/urls.py
+++ interviewproject/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index, name = 'index'),
-    path('users/<str:last_name_prefix>', views.Query_last_name_Prefix, name = 'query_last_name_prefix'),
+    path('users/index', views.Index, name = 'index'),
+    path('users/query/<str:last_name_prefix>', views.Query_last_name_Prefix, name = 'query_last_name_prefix'),
 ]
